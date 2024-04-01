e58932404fadea6cbe986e85720ad631e05b033e
dviross <dvharel12@gmail.com>
2024-04-01 12:12:43 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import "./App.css";
+
+function getRandomAnimal(){
+    const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
+function App(){
+    const [animals, setAnimals] = useState([]);
+    
+    const handelClick = () => {
+        setAnimals([...animals, getRandomAnimal()])
+    }; 
+
+    const renderAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
+    return(
+        <div className="app">
+            <button onClick={handelClick}>Add Animal</button>
+            <div className="animal-list">{renderAnimals}</div>
+        </div>
+    )
+}
+
+export default App;
